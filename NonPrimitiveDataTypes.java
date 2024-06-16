f5c38760f776aaf702423bd482e7f4021421723c
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-06-16 16:27:28 +0530

    binary addition

--- NonPrimitiveDataTypes.java
+++ NonPrimitiveDataTypes.java
@@ -2,7 +2,7 @@ public class NonPrimitiveDataTypes {
     public static void main(String[] args) {
         String str ="test";
         System.out.println("string is"+str);
-        String str1 =new String("test");
+        String str1 =new  String("test");
         System.out.println("new string is"+str1);
         int arr[];
         arr=new int[3];
---
+++ practice.java
@@ -0,0 +1,10 @@
+public class practice{
+    public static void main(String[] args) {
+      double Radius=10;
+        double area=Math.PI* Radius*Radius;
+        System.out.println("area is:"+area);
+    }
+}
+public class practice{
+    
+}
