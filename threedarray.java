59caf519c0621d0b4ae5c83b50b5037b74e55f46
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-06-17 16:55:35 +0530

    3D array

---
+++ threedarray.java
@@ -0,0 +1,6 @@
+public class threedarray{
+    public static void main(String[] args) {
+        int arr[][][]={{{1,2,3},{12,4,5}},{{15,6,7},{17,8,9}}};
+        System.out.println(arr [1][1][2]);
+    }
+}
