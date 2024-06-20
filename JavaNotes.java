9b3b18bd9832a4d36f9f36d3f2124d0390908a55
rajeshpenupothu <rajeshpenupothu@gmail.com>
2024-06-20 16:14:41 +0530

    array of random values

---
+++ JavaNotes.java
@@ -0,0 +1,12 @@
+public class JavaNotes{
+    public static void main(String a[])
+    
+    {int j;
+        for(j=1;j<=5;j++)
+        {
+            System.out.println("hi"+j);
+           
+    }
+    System.out.println("Bye"+j);
+}
+}
